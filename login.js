4ee21ca6087c00ebbd8075c4b572a6071fde68d6
srs4040 <59438129+srs4040@users.noreply.github.com>
2020-04-07 23:44:57 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+if ( username == "Shubhamrajesh007@gmail.com" && password == "mustangride"){
 alert ("Login successfully");
 window.location = "success.html"; // Redirecting to other page.
 return false;
